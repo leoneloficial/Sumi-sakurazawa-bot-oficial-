0f739c4c1dddeddd9e17cff358bcaf8eb6040512
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-05-31 22:42:30 -0400

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -1,17 +1,17 @@
 import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, {conn, usedPrefix, command, text}) => {
-if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese el nombre de la apk para descargarlo.`, m)
+if (!text) return conn.reply(m.chat, `✎ Por favor, ingrese el nombre de la apk para descargarlo.`, m)
 try {
 await m.react(rwait)
-conn.reply(m.chat, `${emoji} Descargando su aplicación, espere un momento...`, m)
+conn.reply(m.chat, `✿ Descargando su aplicación, espere un momento...`, m)
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
 let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
-txt += `☁️ *Nombre* : ${data5.name}\n`
-txt += `🔖 *Package* : ${data5.package}\n`
-txt += `🚩 *Update* : ${data5.lastup}\n`
-txt += `⚖ *Peso* :  ${data5.size}`
+txt += `✎ *Nombre* : ${data5.name}\n`
+txt += `❍ *Package* : ${data5.package}\n`
+txt += `✿ *Update* : ${data5.lastup}\n`
+txt += `❏ *Peso* :  ${data5.size}`
 await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
